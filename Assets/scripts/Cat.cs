ca639c358609d82f08b0c4957243b4a2c8e698da
Rameen Shafiee <rs4047@nyu.edu>
2015-11-03 03:50:15 -0500

    wee

---
+++ Assets/scripts/Cat.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cat : MonoBehaviour {
+
+	public Transform mouse;
+	Rigidbody rb;
+	
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 directionToMouse = mouse.position - transform.position;
+		float angle = Vector3.Angle(transform.forward, directionToMouse);
+		if (angle < 90.0f) {
+			Ray mouseRay = new Ray( transform.position, directionToMouse );
+			RaycastHit mouseRayHitInfo = new RaycastHit();
+			if ( Physics.Raycast ( mouseRay, out mouseRayHitInfo, 50f ) ) {
+				if (mouseRayHitInfo.collider.tag == "Mouse") {
+					rb.AddForce(directionToMouse.normalized * 800f);
+				}
+			}
+		}
+		
+	}
+}
---
+++ Assets/scripts/Mouse.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mouse : MonoBehaviour {
+
+	public Transform cat;
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 directionToCat = cat.position - transform.position;
+		float angle = Vector3.Angle(transform.forward, directionToCat);
+		if (angle < 180.0f) {
+			Ray mouseRay = new Ray( transform.position, directionToCat );
+			RaycastHit mouseRayHitInfo = new RaycastHit();
+		if ( Physics.Raycast ( mouseRay, out mouseRayHitInfo, 40f ) ) {
+			if (mouseRayHitInfo.collider.tag == "Cat") {
+					rb.AddForce(-directionToCat.normalized * 1000f);
+			}
+		}
+	}
+
+}
+}
